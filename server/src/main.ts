4bf876f416b474b857105c1fccc00da3d23e4373
ker4ik13 <kekustop228@mail.ru>
2023-12-16 23:36:23 +0300

    init: установлены зависимости, настроен конфиг и подключение к БД

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3002;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT, () => {
+    console.log(`Cервер запущен на порту ${PORT}.`);
+    if (+PORT === 3333) {
+      console.log('Сервер запущен в режиме разработки.');
+    }
+
+    if (+PORT === 3001) {
+      console.log('Сервер запущен в production режиме.');
+    }
+
+    if (+PORT !== 3001 && +PORT !== 3333) {
+      console.log('Возможно путь к файлу .env указан неправильно.');
+    }
+  });
 }
 bootstrap();
